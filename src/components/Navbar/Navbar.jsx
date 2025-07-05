9a6c3882e51aa21e5c2e384340bd94b288deaabe
Rajshree482 <rajshreebalsamant@gmail.com>
2025-07-05 16:04:00 +0530

    first commit

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,14 +1,30 @@
+import { useState } from 'react'
 import './Navbar.css'
 
 const Navbar = () => {
+  const [isMenuOpen, setIsMenuOpen] = useState(false)
+
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen)
+  }
+
   return (
     <div className='nav'>
       <div className="nav-logo">Koenigsegg</div>
-      <ul className="nav-menu">
-        <li>Home</li>
-        <li>Explore</li>
-        <li>About</li>
-        <li className='nav-contact'>Contact</li>
+      
+      {/* Hamburger Menu for Mobile */}
+      <div className="hamburger" onClick={toggleMenu}>
+        <span></span>
+        <span></span>
+        <span></span>
+      </div>
+
+      {/* Navigation Menu */}
+      <ul className={`nav-menu ${isMenuOpen ? 'active' : ''}`}>
+        <li onClick={() => setIsMenuOpen(false)}>Home</li>
+        <li onClick={() => setIsMenuOpen(false)}>Explore</li>
+        <li onClick={() => setIsMenuOpen(false)}>About</li>
+        <li className='nav-contact' onClick={() => setIsMenuOpen(false)}>Contact</li>
       </ul>
     </div>
   )
