c7b6ba732a9a7d050afd7084b70bda2ea276bda8
Rajshree Balsamant <rajshreebalsamant@gmail.com>
2025-07-14 16:05:46 +0530

    first commit

--- src/components/Background/Background.jsx
+++ src/components/Background/Background.jsx
@@ -1,5 +1,5 @@
 import './Background.css'
-import KoenigseggFamily from '../../Assets/KoenigseggFamily.mp4'
+import Mycar from '../../Assets/Mycar.mp4'
 import gemera1 from '../../Assets/gemera1.png'
 import Jesko from '../../Assets/Jesko.jpg'
 import CC850 from '../../Assets/CC850.jpg'
@@ -10,7 +10,7 @@ const Background = ({playStatus,heroCount}) => {
     if (playStatus) {
          return (
             <video className='background fade-in' autoPlay loop muted>
-                <source src={KoenigseggFamily} type='video/mp4' />
+                <source src={Mycar} type='video/mp4' />
             </video>
          )
     }
